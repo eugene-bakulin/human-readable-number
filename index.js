e692685fe774b08a7b325a0d0197fdd2269a4a9b
eugene-bakulin <bakulin.eugene@gmail.com>
2022-01-07 00:11:16 +0400

    feat: update index.js fix return

--- index.js
+++ index.js
@@ -3,25 +3,29 @@ function toReadable(n){
     const digitsTen = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
     const namesTen = ['twenty', 'thirty', 'fourty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
     if (String(n).length == 1) {
-        console.log(digitsOne[n - 1]);
+        if (+String(n)[0] == 0) {
+            return 'zero';
+        } else {
+            return digitsOne[n - 1];
+        };
     } else if (String(n).length == 2) {
         if (+String(n)[0] == 1) {
-            console.log(digitsTen[+String(n)[1]]);
+            return digitsTen[+String(n)[1]];
         } else {
-            console.log(namesTen[+String(n)[0]-2] + ' ' + digitsOne[+String(n)[1]-1]);
+            return namesTen[+String(n)[0]-2] + ' ' + digitsOne[+String(n)[1]-1];
         }
     } else if ((String(n).length == 3)) {
         if (String(n)[1] == 0) {
-            console.log(digitsOne[+String(n)[0]-1] + ' hundred ' + digitsOne[+String(n)[2]-1]);
+            return digitsOne[+String(n)[0]-1] + ' hundred ' + digitsOne[+String(n)[2]-1];
         } else if (+String(n)[1] == 1) {
-            console.log(digitsOne[+String(n)[0]-1] + ' hundred ' + digitsTen[+String(n)[2]]);
+            return digitsOne[+String(n)[0]-1] + ' hundred ' + digitsTen[+String(n)[2]];
         } else if (+String(n)[1] > 1) {
             if (+String(n)[2] == 0) {
-                console.log(digitsOne[+String(n)[0]-1] + ' hundred ' + namesTen[+String(n)[1]-2]);
+                return digitsOne[+String(n)[0]-1] + ' hundred ' + namesTen[+String(n)[1]-2];
             } else  if (+String(n)[2] != 0) {
-                console.log(digitsOne[+String(n)[0]-1] + ' hundred ' + namesTen[+String(n)[1]-2] + ' ' + digitsOne[+String(n)[2]-1]);
+                return digitsOne[+String(n)[0]-1] + ' hundred ' + namesTen[+String(n)[1]-2] + ' ' + digitsOne[+String(n)[2]-1];
             }
         }
     }
 }
-toReadable(999);
+toReadable(0);
