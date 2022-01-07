9793092e20b1e0b249b356df30debb5198aca2cb
eugene-bakulin <bakulin.eugene@gmail.com>
2022-01-07 23:56:07 +0400

    feat: update

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ function toReadable(n){
     const digitsTen = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
     const namesTen = ['twenty', 'thirty', 'fourty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
     if (String(n).length == 1) {
-        if (+String(n)[0] == 0) {
+        if (+(String(n)[0]) == 0) {
             return 'zero';
         } else {
             return digitsOne[n - 1];
         };
     } else if (String(n).length == 2) {
-        if (+String(n)[0] == 1) {
+        if (+(String(n)[0]) == 1) {
             return digitsTen[+String(n)[1]];
         } else {
             return namesTen[+String(n)[0]-2] + ' ' + digitsOne[+String(n)[1]-1];
@@ -17,13 +17,13 @@ function toReadable(n){
     } else if ((String(n).length == 3)) {
         if (String(n)[1] == 0) {
             return digitsOne[+String(n)[0]-1] + ' hundred ' + digitsOne[+String(n)[2]-1];
-        } else if (+String(n)[1] == 1) {
+        } else if (+(String(n)[1]) == 1) {
             return digitsOne[+String(n)[0]-1] + ' hundred ' + digitsTen[+String(n)[2]];
-        } else if (+String(n)[1] > 1) {
-            if (+String(n)[2] == 0) {
-                return digitsOne[+String(n)[0]-1] + ' hundred ' + namesTen[+String(n)[1]-2];
+        } else if (+(String(n)[1]) > 1) {
+            if (+(String(n)[2]) == 0) {
+                return digitsOne[+(String(n)[0])-1] + ' hundred ' + namesTen[+String(n)[1]-2];
             } else  if (+String(n)[2] != 0) {
-                return digitsOne[+String(n)[0]-1] + ' hundred ' + namesTen[+String(n)[1]-2] + ' ' + digitsOne[+String(n)[2]-1];
+                return digitsOne[+(String(n)[0])-1] + ' hundred ' + namesTen[+String(n)[1]-2] + ' ' + digitsOne[+String(n)[2]-1];
             }
         }
     }
