a32a656eefa990aa271b91aff2ff4d1222ead6f5
eugene-bakulin <bakulin.eugene@gmail.com>
2022-01-08 17:09:39 +0400

    feat: update index.js

--- src/index.js
+++ src/index.js
@@ -11,23 +11,25 @@ module.exports = function toReadable (n) {
         } else if (String(n).length == 2) {
             if (+(String(n).charAt(0)) == 1) {
                 return digitsTen[+(String(n).charAt(1))];
+            } else if (+(String(n).charAt(1)) == 0) {
+                return namesTen[+(String(n).charAt(0)) - 1];
             } else {
-                return namesTen[+(String(n).charAt(0))-2] + ' ' + digitsOne[+(String(n).charAt(1))-1];
+                return namesTen[+(String(n).charAt(0)) - 2] + ' ' + digitsOne[+(String(n).charAt(1))-1];
             }
         } else if ((String(n).length == 3)) {
             if (String(n).charAt(1) == 0) {
                 if (String(n).charAt(2) == 0) {
-                    return digitsOne[+(String(n).charAt(0))-1] + ' hundred';
+                    return digitsOne[+(String(n).charAt(0)) - 1] + ' hundred';
                 } else {
-                    return digitsOne[+(String(n).charAt(0))-1] + ' hundred ' + digitsOne[+(String(n).charAt(2))-1];
+                    return digitsOne[+(String(n).charAt(0)) - 1] + ' hundred ' + digitsOne[+(String(n).charAt(2)) - 1];
                 }
             } else if (+(String(n).charAt(1)) == 1) {
-                return digitsOne[+(String(n).charAt(0))-1] + ' hundred ' + digitsTen[+(String(n).charAt(2))];
+                return digitsOne[+(String(n).charAt(0)) - 1] + ' hundred ' + digitsTen[+(String(n).charAt(2))];
             } else if (+(String(n).charAt(1)) > 1) {
                 if (+(String(n).charAt(2)) == 0) {
-                    return digitsOne[+(String(n).charAt(0))-1] + ' hundred ' + namesTen[+(String(n).charAt(1))-2];
+                    return digitsOne[+(String(n).charAt(0)) - 1] + ' hundred ' + namesTen[+(String(n).charAt(1)) - 2];
                 } else  if (+(String(n).charAt(2)) != 0) {
-                    return digitsOne[+(String(n).charAt(0))-1] + ' hundred ' + namesTen[+(String(n).charAt(1))-2] + ' ' + digitsOne[+(String(n).charAt(2))-1];
+                    return digitsOne[+(String(n).charAt(0)) - 1] + ' hundred ' + namesTen[+(String(n).charAt(1)) - 2] + ' ' + digitsOne[+(String(n).charAt(2)) - 1];
                 }
             }
         }
